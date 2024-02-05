c5f80c899cc19f4f5790483d40e33464ec4ca0d3
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:39:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "krystal-clubconnect-45902",
+  url: "https://krystal-clubconnect-45902.botics.co",
+  api: "https://krystal-clubconnect-45902.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
